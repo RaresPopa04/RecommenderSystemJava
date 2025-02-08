bba11b25161e34808f90d86ec455bb84c555974c
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2025-02-08 10:54:30 +0100

    first commit

---
+++ src/main/java/com/jetbrains/recommendersystemjavabackend/RecommenderSystemJavaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jetbrains.recommendersystemjavabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommenderSystemJavaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecommenderSystemJavaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jetbrains/recommendersystemjavabackend/RecommenderSystemJavaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.recommendersystemjavabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommenderSystemJavaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
