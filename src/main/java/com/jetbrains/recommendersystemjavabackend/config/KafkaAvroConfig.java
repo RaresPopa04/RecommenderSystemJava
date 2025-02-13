dc11be629b35df0c9e4f97ac9d88b591bdb5fd64
Popa Rares Andrei <R.A.Popa@student.tudelft.nl>
2025-02-13 19:24:33 +0100

    Add Kafka Avro configuration for producer
    - Configure Kafka producer with Avro serializer
    - Set up Kafka producer factory and template
    - Use properties for bootstrap servers and schema registry URL

---
+++ src/main/java/com/jetbrains/recommendersystemjavabackend/config/KafkaAvroConfig.java
@@ -0,0 +1,41 @@
+package com.jetbrains.recommendersystemjavabackend.config;
+
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.apache.avro.generic.GenericRecord;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaAvroConfig {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.properties.schema.registry.url}")
+    private String schemaRegistryUrl;
+
+    @Bean
+    public ProducerFactory<String, GenericRecord> avroProducerFactory(){
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put("schema.registry.url", schemaRegistryUrl);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        return new DefaultKafkaProducerFactory<>(props);
+    }
+
+    @Bean
+    public KafkaTemplate<String, GenericRecord> avroKafkaTemplate(){
+        return new KafkaTemplate<>(avroProducerFactory());
+    }
+
+}
